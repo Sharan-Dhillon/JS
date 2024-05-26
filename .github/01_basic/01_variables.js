28869cc672222b25061f8c5aeca408f28936092f
Sharan-Dhillon <sharandeep.nitttr@gmail.com>
2024-05-26 17:26:29 +0000

    github basics

---
+++ .github/01_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountID=148763 
+let accountemail="sharan@google.com"
+var accountpassword="6487"
+accountcity="ludhiana"
+let accountstate;
+
+accountemail="sd@googole.com"
+accountpassword="6752"
+accountcity="delhi"
+//accountID=2
+console.log(accountID);
+console.table([accountID,accountemail,accountpassword,accountcity,accountstate])
---
+++ .github/01_basic/02_datatype.js
@@ -0,0 +1,29 @@
+"use strict" // treat all js code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3+3)
+console.log("sharan")
+
+
+let name="sharan"
+let age=30
+let isloggedIn=true
+
+
+//number=> 2 to power 23
+//bigint
+//string=>""
+//boolean=>t/f
+//null=>standalone value
+//undefined=> value not assignes
+//symbol=>unique
+
+
+//object
+
+
+
+console.log(typeof"sharan")
+console.log(typeof null).              //object
+console.log(typeof undefined);        //undefined
